32a5be7b7707b2dfd005fba189aa467589ddec26
unknown <michaelmolina679@gmail.com>
2023-04-13 19:30:41 -0600

    trying to do something

--- src/components/Home.css/Home.jsx
+++ src/components/Home.css/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 import './Home.css';
+import Timer from '../Timer/Timer';
 
 const Home = () => {
     return(
         <div className='home'>
-            <h1>Aqui empieza todo</h1>
+            <div className="landing flex flex-col justify-center items-center content-center">
+            <h1 className="font-bold">ODS</h1>
+            <h3 className='subtitle uppercase font-medium'>Para cumplir con la meta de 2023</h3>
+            <Timer />
+        </div>
+        <Button variant='primary'>Iniciar</Button>
         </div>
     )
 }
---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+
+import './Timer.css';
+
+const Timer = () => {
+    const [days, setDays] = useState(0);
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+
+    const deadline = "December, 31, 2023";
+
+    const getTime = () => {
+        const time = Date.parse(deadline) - Date.now();
+
+        setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
+        setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
+        setMinutes(Math.floor((time / 1000 / 60) % 60));
+        setSeconds(Math.floor((time / 1000) % 60));
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => getTime(deadline), 1000);
+    
+        return () => clearInterval(interval);
+      }, []);
+
+    return (
+        <div className="img-bg">
+            <div className="timer flex justify-items-center" role="timer" >
+                <div className="col-4">
+                    <div className="box">
+                        <p className='text-3xl font-semibold' id="day">{days < 10 ? `0 ${days}` : days}</p>
+                        <span className="text font-medium">Days</span>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <div className="box">
+                        <p className='text-3xl font-semibold' id="hour">{hours < 10 ? `0 ${hours}` : hours}</p>
+                        <span className="text font-medium">Hours</span>
+                    </div>
+                </div>
+                <div className="col-4">
+                    <div className="box">
+                        <p className='text-3xl font-semibold' id="minute">{minutes < 10 ? `0 ${minutes}` : minutes}</p>
+                        <span className="text font-medium">Minutes</span>
+                    </div>
+                </div>
+
+                <div className="col-4">
+                    <div className="box">
+                        <p className='text-3xl font-semibold' id="second">{seconds < 10 ? `0 ${seconds}` : seconds}</p>
+                        <span className="text font-medium">Seconds</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Timer;
