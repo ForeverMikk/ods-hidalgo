3609fc6dafef51970514a3cf54841047661ee4f8
unknown <michaelmolina679@gmail.com>
2023-04-14 19:12:45 -0600

    adding media quierys

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -12,13 +12,13 @@ const Content = () => {
 
             <div className="objetivos">
                 <div className="text">
-                    <img src={objetivosImg} alt="Objetivos del Programa" />
+                    <img className='main-img' src={objetivosImg} alt="Objetivos del Programa" />
                     <h2>Crecemos con base en objetuvos</h2>
                     <br />
                     <p>"Objetivos de Desarrollo Sostenible", un conjunto de 17 objetivos interconenctados establecidos por las Naciones Unidas en 2015 como parte de la Agenda 2030 para Desarrollo Sostenible.</p>
                     <p>Los ODS buscan abordar los principales desafíos mundiales, como la pobreza, la desigualdad, el cambio climático y la degradación ambiental, entre otros, y establecer metas claras y medibles para lograr un futuro sotenible para todos.</p>
                     <br />
-                    <img src={iconoGob} alt="Icono de Hidalgo"/>
+                    <img className='icon' src={iconoGob} alt="Icono de Hidalgo"/>
                 </div>
             </div>
 
