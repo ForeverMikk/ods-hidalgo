5abb37557dfef48a5fd0fcb22844b5b17ec8c487
unknown <michaelmolina679@gmail.com>
2023-04-18 18:19:07 -0600

    adding all images

--- src/variables/objetivos.js
+++ src/variables/objetivos.js
@@ -1,8 +1,8 @@
 import sencibilizacion from '../assets/img/sencibilizacion.png';
-// import respaldo from '../assets/img/Recurso 14.png';
-// import implementacion from '../assets/img/Recurso 15.png';
-// import direccion from '../assets/img/Recurso 17.png';
-// import monitoreo from '../assets/img/Recurso 18.png';
+import respaldo from '../assets/img/respaldo.png';
+import implementacion from '../assets/img/implementacion.png';
+import direccion from '../assets/img/direccion.png';
+import monitoreo from '../assets/img/monitoreo.png';
 
 export const objetivos = [
     {
@@ -10,6 +10,7 @@ export const objetivos = [
         description: "La difusión y divulgación de los ODS es importante para todos los niveles de gobierno, desde el federal hasta el municipal, y en los 3 poderes del gobierno: ejecutivo, legislativo y judicial",
         subtitle: "Sector Público, iniciativa privada, academia, sociedad civil y medio ambiente",
         img: sencibilizacion,
+        className: "normal",
         data: [
             {
                 title: "Compromisos",
@@ -37,8 +38,95 @@ export const objetivos = [
         title: "Sensibilización",
         description: "La difusión y divulgación de los ODS es importante para todos los niveles de gobierno, desde el federal hasta el municipal, y en los 3 poderes del gobierno: ejecutivo, legislativo y judicial",
         subtitle: "Sector Público, iniciativa privada, academia, sociedad civil y medio ambiente",
-        img: sencibilizacion,
+        img: respaldo,
         className: "reverse",
+        data: [
+            {
+                title: "Compromisos",
+                aspects: [
+                    "Firma de convenios",
+                    "Grupos intersecretariales"
+                ]
+            },
+            {
+                title: "Sentido de aproiación",
+                aspects: [
+                    "Mi meta, mi planeta",
+                ]
+            },
+            {
+                title: "Corresponsabilidades",
+                aspects: [
+                    "Plan de trabajo anual",
+                    "Mesas de Trabajo con lideres empresariales"
+                ]
+            }
+        ]
+    },
+    {
+        title: "Sensibilización",
+        description: "La difusión y divulgación de los ODS es importante para todos los niveles de gobierno, desde el federal hasta el municipal, y en los 3 poderes del gobierno: ejecutivo, legislativo y judicial",
+        subtitle: "Sector Público, iniciativa privada, academia, sociedad civil y medio ambiente",
+        img: implementacion,
+        className: "normal",
+        data: [
+            {
+                title: "Compromisos",
+                aspects: [
+                    "Firma de convenios",
+                    "Grupos intersecretariales"
+                ]
+            },
+            {
+                title: "Sentido de aproiación",
+                aspects: [
+                    "Mi meta, mi planeta",
+                ]
+            },
+            {
+                title: "Corresponsabilidades",
+                aspects: [
+                    "Plan de trabajo anual",
+                    "Mesas de Trabajo con lideres empresariales"
+                ]
+            }
+        ]
+    },
+    {
+        title: "Sensibilización",
+        description: "La difusión y divulgación de los ODS es importante para todos los niveles de gobierno, desde el federal hasta el municipal, y en los 3 poderes del gobierno: ejecutivo, legislativo y judicial",
+        subtitle: "Sector Público, iniciativa privada, academia, sociedad civil y medio ambiente",
+        img: direccion,
+        className: "reverse",
+        data: [
+            {
+                title: "Compromisos",
+                aspects: [
+                    "Firma de convenios",
+                    "Grupos intersecretariales"
+                ]
+            },
+            {
+                title: "Sentido de aproiación",
+                aspects: [
+                    "Mi meta, mi planeta",
+                ]
+            },
+            {
+                title: "Corresponsabilidades",
+                aspects: [
+                    "Plan de trabajo anual",
+                    "Mesas de Trabajo con lideres empresariales"
+                ]
+            }
+        ]
+    },
+    {
+        title: "Sensibilización",
+        description: "La difusión y divulgación de los ODS es importante para todos los niveles de gobierno, desde el federal hasta el municipal, y en los 3 poderes del gobierno: ejecutivo, legislativo y judicial",
+        subtitle: "Sector Público, iniciativa privada, academia, sociedad civil y medio ambiente",
+        img: monitoreo,
+        className: "normal",
         data: [
             {
                 title: "Compromisos",
